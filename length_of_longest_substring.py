c805843cb573b6a86ded08c2e6f7050b9c6ae36e
willtucker42 <willtucker42@gmail.com>
2022-02-14 22:01:38 -0800

    Create length_of_longest_substring.py

---
+++ length_of_longest_substring.py
@@ -0,0 +1,22 @@
+# Given a string s, find the length of the longest substring without repeating characters.
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        temp_s = ''
+        longest_temp = 0
+        longest = 0
+        start = 0
+        while True:
+            if start == len(s):
+                break
+            for c in s[start:]:
+                if c not in temp_s:
+                    temp_s += c
+                    longest_temp += 1
+                else:
+                    if longest_temp >= longest:
+                        longest = longest_temp
+                    temp_s = ''
+                    start += 1
+                    longest_temp = 0
+                    break
+        return longest
